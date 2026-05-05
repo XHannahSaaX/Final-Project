66c7ab93e8d8a0c7f6589dbaaf1b758abe30b62e
hanna <hcs230002@utdallas.edu>
2026-05-05 15:15:10 -0500

    Added a Class for particles that's similar to that from the digital rain project. I have the init and the update loop for the class so far.

--- Cursor.py
+++ Cursor.py
@@ -2,6 +2,36 @@ import math
 import pygame
 import random
 
+class Particle():
+
+    def __init__(self, pos=(0, 0), size=15, life=1000, color=None, shape="circ"):
+        self.pos = pos
+        self.size = size
+        self.age = 0
+        self.life = life
+        self.dead = False
+        self.shape = shape
+        self.vel = [random.uniform(-1, 1)]
+
+        if color is None:
+            self.color = pygame.Color(random.randrange(0, 255),
+                                   random.randrange(0, 255),
+                                    random.randrange(0, 255))
+        else:
+            self.color = color
+
+        self.alpha = 255
+        self.surface = self.update_surface()
+
+    def update(self, dt):
+        self.age += dt
+        if self.age > self.life:
+            #print("Particle is dead")
+            self.dead = True
+
+        self.vel[1] += 0.5
+        self.alpha = 255 * (1 - (self.age / self.life))
+
 
 def main():
 
